8d5fdb8e85dfc3c1f8be7843935f533a8bcbb82f
Valleon <val.ekechukwu@gmail.com>
2022-09-27 15:46:47 +0100

    Implemented the business logic for student registration.

--- src/main/java/com/valleon/rewardyourteacherapi/service/impl/RegisterServiceImpl.java
+++ src/main/java/com/valleon/rewardyourteacherapi/service/impl/RegisterServiceImpl.java
@@ -3,16 +3,22 @@ package com.valleon.rewardyourteacherapi.service.impl;
 import com.valleon.rewardyourteacherapi.domain.dao.AppUserDao;
 import com.valleon.rewardyourteacherapi.domain.dao.SchoolDao;
 import com.valleon.rewardyourteacherapi.domain.dao.StudentDao;
-import com.valleon.rewardyourteacherapi.domain.dao.TeacherDao;
 import com.valleon.rewardyourteacherapi.domain.entities.AppUser;
+import com.valleon.rewardyourteacherapi.domain.entities.School;
+import com.valleon.rewardyourteacherapi.domain.entities.Student;
+import com.valleon.rewardyourteacherapi.domain.entities.enums.Role;
+import com.valleon.rewardyourteacherapi.infrastructure.exceptionHandlers.CustomNotFoundException;
+import com.valleon.rewardyourteacherapi.infrastructure.exceptionHandlers.EntityAlreadyExistException;
 import com.valleon.rewardyourteacherapi.service.RegisterService;
 import com.valleon.rewardyourteacherapi.service.payload.request.StudentRegistrationRequest;
-import com.valleon.rewardyourteacherapi.service.payload.response.APIResponse;
+import com.valleon.rewardyourteacherapi.service.payload.response.ApiResponse;
 import com.valleon.rewardyourteacherapi.service.payload.response.RegistrationResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -22,20 +28,41 @@ public class RegisterServiceImpl implements RegisterService {
     private final StudentDao studentDao;
     private final SchoolDao schoolDao;
 
-    private final APIResponse registrationResponse;
+    private final ApiResponse registrationResponse;
 
     private final AppUserDao appUserDao;
 
-    private final TeacherDao teacherDao;
+    private final PasswordEncoder passwordEncoder;
+
+//    private final TeacherDao teacherDao;
 
 
     @Override
     public RegistrationResponse registerStudent(StudentRegistrationRequest studentRegistrationRequest) {
         Optional<AppUser> appUser = appUserDao.findAppUserByEmail(studentRegistrationRequest.getEmail());
-        if(appUser.isPresent()){
-            throw new RuntimeException();
+        if (appUser.isPresent()) {
+            throw new EntityAlreadyExistException("Email has already been registered");
         }
 
+        School school = schoolDao.findSchool(studentRegistrationRequest.getSchoolName())
+                .orElseThrow(() -> new CustomNotFoundException("School not found."));
+
+        AppUser appUser1 = AppUser.builder()
+                .email(studentRegistrationRequest.getEmail())
+                .password(passwordEncoder.encode(studentRegistrationRequest.getPassword()))
+                .role(Role.STUDENT)
+                .build();
+
+        AppUser appUser2 = appUserDao.saveRecord(appUser1);
+        Student student = Student.builder()
+                .name(studentRegistrationRequest.getName())
+                .phoneNumber(studentRegistrationRequest.getPhoneNumber())
+                .school(school)
+                .appUser(appUser2)
+//                .displayPicture(studentRegistrationRequest.)
+                .build();
+
+        return registrationResponse.created("Success", LocalDateTime.now(), studentDao.saveRecord(student));
         //        Optional<AppUser> appUser = appUserDao.findAppUserByEmail(studentRegistrationRequest.getEmail());
 //        if (appUser.isPresent()) {
 //            throw new EntityAlreadyExistException("Email already taken");
@@ -59,6 +86,6 @@ public class RegisterServiceImpl implements RegisterService {
 //                .build();
 //
 //        return registrationResponse.created("Success", LocalDateTime.now(), studentDao.saveRecord(student));
-        return null;
+
     }
 }
