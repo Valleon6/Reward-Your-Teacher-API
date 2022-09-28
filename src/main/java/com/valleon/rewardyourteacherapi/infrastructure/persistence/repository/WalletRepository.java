8943a64e09c51195e48a506b14b4d1533743904a
Valleon <val.ekechukwu@gmail.com>
2022-09-29 00:30:51 +0100

    Created repositories for Chat, Notification,Transaction and wallet entities.

---
+++ src/main/java/com/valleon/rewardyourteacherapi/infrastructure/persistence/repository/WalletRepository.java
@@ -0,0 +1,14 @@
+package com.valleon.rewardyourteacherapi.infrastructure.persistence.repository;
+
+import com.valleon.rewardyourteacherapi.domain.entities.Student;
+import com.valleon.rewardyourteacherapi.domain.entities.Teacher;
+import com.valleon.rewardyourteacherapi.domain.entities.transact.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    Optional<Wallet> findWalletByTeacher(Teacher teacher);
+
+    Optional<Wallet> findWalletByStudent(Student student);
+}
