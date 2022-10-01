d54b8735d9dce8acc454639c9838098c4f03e893
Valleon <val.ekechukwu@gmail.com>
2022-10-01 22:00:48 +0100

    Implemented the CloudinaryService class.

---
+++ src/main/java/com/valleon/rewardyourteacherapi/utilities/CloudinaryService.java
@@ -0,0 +1,53 @@
+package com.valleon.rewardyourteacherapi.utilities;
+
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    private static File convertMultipartToFile(MultipartFile image) throws IOException{
+        File convertedFile;
+        String file = image.getOriginalFilename();
+
+        assert file != null;
+
+      try (  FileOutputStream fileOutputStream = new FileOutputStream(file)){
+            convertedFile = new File(file);
+            fileOutputStream.write(image.getBytes());
+        }
+      return convertedFile;
+    }
+
+    public String uploadImage(MultipartFile image) throws IOException{
+        try{
+            File uploadedFile = convertMultipartToFile(image);
+            Map uploadResult = cloudinary.uploader().upload(uploadedFile,
+                    ObjectUtils.asMap("use filename", true, "unique-filename", true));
+            boolean isDeleted = uploadedFile.delete();
+
+            if(isDeleted)
+                log.info("File Deleted Successfully");
+            else
+                log.info("File does not exist");
+            return uploadResult.get("url").toString();
+        }catch (IOException e){
+            throw new IOException(e);
+        }
+    }
+
+}
