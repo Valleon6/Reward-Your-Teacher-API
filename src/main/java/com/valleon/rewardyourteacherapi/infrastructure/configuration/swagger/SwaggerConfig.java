060dd3abab3923671eda4bf8d803bd1e7f50b470
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-07 17:29:56 +0100

    Modifying the Swagger configuration

--- src/main/java/com/valleon/rewardyourteacherapi/infrastructure/configuration/swagger/SwaggerConfig.java
+++ src/main/java/com/valleon/rewardyourteacherapi/infrastructure/configuration/swagger/SwaggerConfig.java
@@ -1,21 +1,90 @@
 package com.valleon.rewardyourteacherapi.infrastructure.configuration.swagger;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.mappers.LicenseMapper;
 
 @Configuration
 @EnableSwagger2WebMvc
+@ConditionalOnProperty(name = "springdoc.swagger-ui.enabled", havingValue = "true",matchIfMissing = true)
 public class SwaggerConfig {
+
+    @Value("${api.info.title: Reward Your Teacher}")
+    private String title;
+
+    @Value("${api.info.description: api.info.description}")
+    private String description;
+
+    @Value("${api.info.version: v1}")
+    private String version;
+
+    @Value("${api.info.term-of-service: Terms}")
+    private String termOfService;
+
+    @Value("${api.info.contact.name: Valentine Ekechukwu}")
+    private String contactName;
+
+    @Value("${api.info.contact.email: val.ekechukwu@gmail.com}")
+    private String contactEmail;
+
+    @Value("${api.info.contact.url: github/valleon}")
+    private String contactUrl;
+
+    @Value("${api.info.license.name: api.info.license.name}")
+    private String licenseName;
+
+    @Value("${api.info.license.url: api.info.license.url")
+    private String licenseUrl;
+
+    private Info getApiInfo(){
+        Contact contact = new Contact().name(contactName).email(contactEmail).url(contactUrl);
+        License license = new License().name(licenseName).url(licenseUrl);
+
+        return new Info()
+                .title(title)
+                .description(description)
+                .version(version)
+                .termsOfService(termOfService)
+                .contact(contact)
+                .license(license);
+    }
+
+    @Bean
+    public OpenAPI productApi(){
+        return new OpenAPI()
+                .info(getApiInfo());
+    }
+
+
+    private ApiInfo fetchApiInfo(){
+        springfox.documentation.service.Contact contact = new springfox.documentation.service.Contact(contactName,contactUrl,contactEmail);
+        LicenseMapper.License license = new License(licenseName, licenseUrl);
+
+        return new ApiInfo(title, description, version, termOfService, contact, license, licenseUrl  );
+//                .title(title)
+//                .description(description)
+//                .version(version)
+//                .contact(contact)
+//                .license(license);
+    }
     //Creating a Docket bean
     // builder which is intended to be the primary interface into the swagger-springmvc framework
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(fetchApiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
