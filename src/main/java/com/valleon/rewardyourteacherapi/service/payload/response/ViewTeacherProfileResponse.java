1a3c71496f461759edbc3b89aabd6b1467c1184e
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-12 19:28:20 +0100

    Update ViewTeacherProfileResponse.java

--- src/main/java/com/valleon/rewardyourteacherapi/service/payload/response/ViewTeacherProfileResponse.java
+++ src/main/java/com/valleon/rewardyourteacherapi/service/payload/response/ViewTeacherProfileResponse.java
@@ -3,10 +3,12 @@ package com.valleon.rewardyourteacherapi.service.payload.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ViewTeacherProfileResponse {
     private String name;
     private String email;
