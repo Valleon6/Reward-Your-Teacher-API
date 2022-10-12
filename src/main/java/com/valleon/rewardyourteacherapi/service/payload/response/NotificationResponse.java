e678ba399c74f579f521ac03e4898a8297ec2eef
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-12 19:27:08 +0100

    Create NotificationResponse.java

---
+++ src/main/java/com/valleon/rewardyourteacherapi/service/payload/response/NotificationResponse.java
@@ -0,0 +1,12 @@
+package com.valleon.rewardyourteacherapi.service.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class NotificationResponse {
+    private String message;
+}
