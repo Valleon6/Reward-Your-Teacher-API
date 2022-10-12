c54e20027bcdae71d8f87af99b5b079c80532aa7
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-12 18:29:17 +0100

    Create NotificationType.java

---
+++ src/main/java/com/valleon/rewardyourteacherapi/domain/entities/enums/NotificationType.java
@@ -0,0 +1,7 @@
+package com.valleon.rewardyourteacherapi.domain.entities.enums;
+
+public enum NotificationType {
+    CREDIT_NOTIFICATION,
+    DEBIT_NOTIFICATION,
+    APPRECIATION_NOTIFICATION
+}
