65947dbebe93940e358664eb5918d203a11274e7
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-12 15:34:02 +0100

    Updated the AppUserDao Interface and added a new method.

--- src/main/java/com/valleon/rewardyourteacherapi/domain/dao/AppUserDao.java
+++ src/main/java/com/valleon/rewardyourteacherapi/domain/dao/AppUserDao.java
@@ -1,10 +1,14 @@
 package com.valleon.rewardyourteacherapi.domain.dao;
 
 import com.valleon.rewardyourteacherapi.domain.entities.AppUser;
+import com.valleon.rewardyourteacherapi.domain.entities.enums.Role;
 
 import java.util.Optional;
 
 public interface AppUserDao extends CrudDAO<AppUser, Long>{
 
-    Optional<AppUser> findAppUserByEmail(String email);
+    AppUser findAppUserByEmail(String email);
+
+    AppUser findAppUserByEmailAndRole(String email, Role role);
 }
+
