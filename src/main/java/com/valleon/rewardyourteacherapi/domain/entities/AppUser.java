11a536387705ebe6660eaf73b7d6282813d614d0
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-12 14:54:01 +0100

    Update AppUser.java

--- src/main/java/com/valleon/rewardyourteacherapi/domain/entities/AppUser.java
+++ src/main/java/com/valleon/rewardyourteacherapi/domain/entities/AppUser.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "ryt_app_user")
+@Table(name = "app_user")
 public class AppUser extends AbstractEntity {
 
     @Column(nullable = false, unique = true, columnDefinition = "VARCHAR(100)")
