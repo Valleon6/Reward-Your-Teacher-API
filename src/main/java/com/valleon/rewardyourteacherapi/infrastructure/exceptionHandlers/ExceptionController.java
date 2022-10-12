b0466c230d2170dda12f8118f2679dc405d430e2
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-12 19:55:12 +0100

    Update ExceptionController.java

--- src/main/java/com/valleon/rewardyourteacherapi/infrastructure/exceptionHandlers/ExceptionController.java
+++ src/main/java/com/valleon/rewardyourteacherapi/infrastructure/exceptionHandlers/ExceptionController.java
@@ -1,5 +1,6 @@
 package com.valleon.rewardyourteacherapi.infrastructure.exceptionHandlers;
 
+import com.valleon.rewardyourteacherapi.service.payload.response.ApiResponse;
 import com.valleon.rewardyourteacherapi.service.payload.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,31 +8,48 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.io.IOException;
+
 @ControllerAdvice
 public class ExceptionController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CustomNotFoundException.class)
     public ResponseEntity<?> UserNotFoundException(CustomNotFoundException ex) {
-        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), false), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ApiResponse<>((ex.getMessage()), false), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(EntityAlreadyExistException.class)
     public ResponseEntity<?> EntityAlreadyExistException(EntityAlreadyExistException ex) {
-        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), false), HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(new ApiResponse<>((ex.getMessage()), false), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(AuthenticationFailedException.class)
     public ResponseEntity<?> AuthenticationFailedExceptionHandler(AuthenticationFailedException ex) {
-        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), false), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ApiResponse<>((ex.getMessage()), false), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(JwtInvalidException.class)
-    public ResponseEntity<?> JwtInvalidException(JwtInvalidException ex) {
-        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), false), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> JwtExceptionHandler(JwtInvalidException ex) {
+        return new ResponseEntity<>(new ApiResponse<>((ex.getMessage()), false), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<?> IllegalJwtExceptionHandler(IllegalArgumentException ex){
-        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), false), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ApiResponse<>((ex.getMessage()), false), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<?> IOException(IOException ex) {
+        return new ResponseEntity<>(new ApiResponse<>(ex.getMessage(), false), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<?> InsufficientBalanceExceptionHandler(InsufficientBalanceException ex) {
+        return new ResponseEntity<>(new ApiResponse<>(ex.getMessage(), false), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotificationNotFoundException.class)
+    public ResponseEntity<?> NotificationNotFoundException(NotificationNotFoundException ex) {
+        return new ResponseEntity<>(new ApiResponse<>(ex.getMessage(), false), HttpStatus.NOT_FOUND);
     }
 }
