e61e130e86e2139f33d99e128384d6e75d06c78e
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-12 18:46:08 +0100

    Update EditProfileResponse.java

--- src/main/java/com/valleon/rewardyourteacherapi/service/payload/response/EditProfileResponse.java
+++ src/main/java/com/valleon/rewardyourteacherapi/service/payload/response/EditProfileResponse.java
@@ -1,13 +1,21 @@
 package com.valleon.rewardyourteacherapi.service.payload.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class EditProfileResponse {
-    private String message;
-    private LocalDateTime timeStamp;
+//    private String message;
+//    private LocalDateTime timeStamp;
+
+    private String name;
+    private String email;
+    private long id;
 }
