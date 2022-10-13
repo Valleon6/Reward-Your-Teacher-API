aac4d963865caef032ff958aa302828ed11bdf60
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-13 11:26:06 +0100

    Create ConfirmationTokenServiceImpl.java

---
+++ src/main/java/com/valleon/rewardyourteacherapi/service/impl/ConfirmationTokenServiceImpl.java
@@ -0,0 +1,35 @@
+package com.valleon.rewardyourteacherapi.service.impl;
+
+import com.valleon.rewardyourteacherapi.domain.dao.ConfirmationTokenDao;
+import com.valleon.rewardyourteacherapi.domain.entities.email.ConfirmationTokenEntity;
+import com.valleon.rewardyourteacherapi.service.payload.ConfirmationTokenService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@Getter
+@Setter
+@AllArgsConstructor
+public class ConfirmationTokenServiceImpl implements ConfirmationTokenService {
+
+    private final ConfirmationTokenDao confirmationTokenDao;
+
+    @Override
+    public void saveConfirmationToken(ConfirmationTokenEntity token) {
+        confirmationTokenDao.saveRecord(token);
+    }
+
+    @Override
+    public ConfirmationTokenEntity getToken(String token) {
+        return confirmationTokenDao.findByToken(token);
+    }
+
+    @Override
+    public int setConfirmedAt(String token) {
+        return confirmationTokenDao.updateConfirmedAt(token, LocalDateTime.now());
+    }
+}
