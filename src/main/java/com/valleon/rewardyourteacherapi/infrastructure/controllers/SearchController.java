3de77cc645f11794c339ef7f44d9652d3f07340c
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-13 23:28:37 +0100

    Merge the Teacher and school SearchController class into a new  one
    Merge the Teacher and school SearchController class into a new Controller class and deleted  the redundant searchController classes.

---
+++ src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/SearchController.java
@@ -0,0 +1,44 @@
+package com.valleon.rewardyourteacherapi.infrastructure.controllers;
+
+import com.valleon.rewardyourteacherapi.service.payload.SearchService;
+import com.valleon.rewardyourteacherapi.service.payload.response.ApiResponse;
+import com.valleon.rewardyourteacherapi.service.payload.response.SchoolSearchResponse;
+import com.valleon.rewardyourteacherapi.service.payload.response.TeacherSearchResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/v1/search")
+@AllArgsConstructor
+public class SearchController {
+
+    private final SearchService service;
+
+    @GetMapping("/schools")
+    public ResponseEntity<ApiResponse<Set<SchoolSearchResponse>>> getAllSchools(@RequestParam("offset") int offset, @RequestParam("pageSize") int pageSize) {
+        return new ResponseEntity(new ApiResponse<>("success",true,service.findAllSchools(offset, pageSize)), HttpStatus.FOUND);
+    }
+    @GetMapping("/teachers/{schoolName}")
+    public ResponseEntity<ApiResponse<List<TeacherSearchResponse>>> findAllTeacherInASchool(
+            @RequestParam("offset") int offset,
+            @RequestParam("pageSize") int pageSize,
+            @PathVariable("schoolName") String schoolName
+    ) {
+        return new ResponseEntity<>(new ApiResponse<>("success",true,service.findAllTeachersInASchool(offset, pageSize, schoolName)), HttpStatus.FOUND);
+    }
+
+    @GetMapping("/teachers")
+    public ResponseEntity<ApiResponse<List<TeacherSearchResponse>>> getAllTeachers(@RequestParam("offset") int offset, @RequestParam("pageSize") int pageSize) {
+        return new ResponseEntity<>(new ApiResponse<>("success",true,service.getAllTeachers(offset, pageSize)), HttpStatus.FOUND);
+    }
+
+    @GetMapping("/teacher/{teacherName}")
+    public ResponseEntity<ApiResponse<List<TeacherSearchResponse>>> findTeacher(@PathVariable("teacherName") String keyword) {
+        return new ResponseEntity<>(new ApiResponse<>("success",true,service.searchTeacherByName(keyword)), HttpStatus.FOUND);
+    }
+}
