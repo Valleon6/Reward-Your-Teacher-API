dfb0c26fd75a21ea6d6cf7f0c62ea5c04c21bf5f
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-13 22:58:06 +0100

    Update RegisterController.java

--- src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/RegisterController.java
+++ src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/RegisterController.java
@@ -1,8 +1,10 @@
 package com.valleon.rewardyourteacherapi.infrastructure.controllers;
 
+import com.google.gson.Gson;
 import com.valleon.rewardyourteacherapi.service.payload.RegisterService;
 import com.valleon.rewardyourteacherapi.service.payload.request.StudentRegistrationRequest;
 import com.valleon.rewardyourteacherapi.service.payload.request.TeacherRegistrationRequest;
+import com.valleon.rewardyourteacherapi.service.payload.response.ApiResponse;
 import com.valleon.rewardyourteacherapi.service.payload.response.RegistrationResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,9 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-
 import javax.validation.Valid;
-import java.io.IOException;
 
 @RestController
 @AllArgsConstructor
@@ -22,13 +22,22 @@ public class RegisterController {
     private final RegisterService registerService;
 
     @PostMapping(value = "/student")
-    public ResponseEntity<RegistrationResponse> registerStudent(@Valid @RequestBody StudentRegistrationRequest studentRegistrationRequest) {
-        return new ResponseEntity<>(registerService.registerStudent(studentRegistrationRequest), HttpStatus.CREATED);
+    public ResponseEntity<ApiResponse<RegistrationResponse>> registerStudent(@Valid @RequestBody StudentRegistrationRequest studentRegistrationRequest)throws Exception {
+        RegistrationResponse response = registerService.registerStudent(studentRegistrationRequest);
+        return new ResponseEntity<>(new ApiResponse<>("Registered successfully", true, response), HttpStatus.CREATED);
     }
 
     @PostMapping(value = "/teacher")
-    public ResponseEntity<RegistrationResponse> registerTeacher(@Valid TeacherRegistrationRequest teacherRegistrationRequest, @RequestPart MultipartFile file) throws IOException {
-        return new ResponseEntity<>(registerService.registerTeacher(teacherRegistrationRequest, file), HttpStatus.CREATED);
+    public ResponseEntity<ApiResponse<RegistrationResponse>> registerTeacher(@Valid @RequestParam("registration") String registration, @RequestParam("file") MultipartFile file) throws Exception {
+        TeacherRegistrationRequest teacherRegistrationRequest = new Gson().fromJson(registration,TeacherRegistrationRequest.class);
+        RegistrationResponse response =  registerService.registerTeacher( teacherRegistrationRequest,file);
+        return new ResponseEntity<>(new ApiResponse<>("Registered Successfully",true,response), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/verification")
+    public ResponseEntity<ApiResponse<RegistrationResponse>> verifyUser(@Valid @RequestParam("token") String token) throws Exception {
+        registerService.verifyUser(token);
+        return new ResponseEntity<>(new ApiResponse<>("Registered Successfully",true), HttpStatus.CREATED);
     }
 
 }
