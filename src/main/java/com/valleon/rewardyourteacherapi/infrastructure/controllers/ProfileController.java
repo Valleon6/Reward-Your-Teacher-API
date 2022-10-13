288076d587a21ba12c5cb4ca3822103d6fc6b3e9
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-13 22:49:40 +0100

    Create new ProfileController class to replace old redundant one

---
+++ src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/ProfileController.java
@@ -0,0 +1,44 @@
+package com.valleon.rewardyourteacherapi.infrastructure.controllers;
+
+import com.valleon.rewardyourteacherapi.service.payload.ProfileService;
+import com.valleon.rewardyourteacherapi.service.payload.request.StudentProfileRequest;
+import com.valleon.rewardyourteacherapi.service.payload.request.TeacherProfileRequest;
+import com.valleon.rewardyourteacherapi.service.payload.response.ApiResponse;
+import com.valleon.rewardyourteacherapi.service.payload.response.EditProfileResponse;
+import com.valleon.rewardyourteacherapi.service.payload.response.ViewTeacherProfileResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/profile")
+public class ProfileController {
+    private final ProfileService studentProfileService;
+    private final ProfileService teacherProfileService;
+
+    @PutMapping("/edit/student")
+    public ResponseEntity<ApiResponse<EditProfileResponse>> editStudentProfile(@Valid @RequestBody StudentProfileRequest studentProfileRequest) {
+        EditProfileResponse editProfileResponse = studentProfileService.editStudentProfile(studentProfileRequest);
+
+        return new ResponseEntity<>(new ApiResponse<>("Edited successfully", true, editProfileResponse), HttpStatus.OK);
+    }
+
+    @PutMapping("/edit/teacher")
+    public ResponseEntity<ApiResponse<EditProfileResponse>>editTeacherProfile(@Valid @RequestBody TeacherProfileRequest teacherProfileRequest, MultipartFile file) throws IOException {
+        EditProfileResponse editProfileResponse =  teacherProfileService.editTeacherProfile(teacherProfileRequest,file);
+        return new ResponseEntity<>(new ApiResponse<>("Edited successfully",true,editProfileResponse),HttpStatus.OK);
+    }
+
+    @GetMapping("/view/teacher/{teacherName}")
+    public ResponseEntity<ApiResponse<List<ViewTeacherProfileResponse>>> viewTeacherProfile(@PathVariable("teacherName") String name) {
+        return new ResponseEntity<>(new ApiResponse<>("success",true,teacherProfileService.viewTeacherByName(name)), HttpStatus.FOUND);
+    }
+
+}
