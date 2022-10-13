43e52db42b52f9553c1b38080740bf0ee22d3f5b
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-13 11:26:16 +0100

    Update ExceptionController.java

--- src/main/java/com/valleon/rewardyourteacherapi/infrastructure/exceptionHandlers/ExceptionController.java
+++ src/main/java/com/valleon/rewardyourteacherapi/infrastructure/exceptionHandlers/ExceptionController.java
@@ -1,9 +1,9 @@
 package com.valleon.rewardyourteacherapi.infrastructure.exceptionHandlers;
 
 import com.valleon.rewardyourteacherapi.service.payload.response.ApiResponse;
-import com.valleon.rewardyourteacherapi.service.payload.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -51,5 +51,10 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NotificationNotFoundException.class)
     public ResponseEntity<?> NotificationNotFoundException(NotificationNotFoundException ex) {
         return new ResponseEntity<>(new ApiResponse<>(ex.getMessage(), false), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MailException.class)
+    public ResponseEntity<?> MailException(MailException ex) {
+        return new ResponseEntity<>(new ApiResponse<>(ex.getMessage(), false), HttpStatus.BAD_GATEWAY);
     }
 }
