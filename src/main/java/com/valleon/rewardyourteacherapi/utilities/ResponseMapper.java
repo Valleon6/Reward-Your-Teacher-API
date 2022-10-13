8240d29d9b36da7b4de2ef5f0359d2deb42d2854
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-13 18:54:48 +0100

    Create ResponseMapper.java

---
+++ src/main/java/com/valleon/rewardyourteacherapi/utilities/ResponseMapper.java
@@ -0,0 +1,26 @@
+package com.valleon.rewardyourteacherapi.utilities;
+
+import com.valleon.rewardyourteacherapi.domain.entities.Teacher;
+import com.valleon.rewardyourteacherapi.service.payload.response.TeacherSearchResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResponseMapper {
+
+    public TeacherSearchResponse teacherSearchResponseToTeacherMapper(Teacher teacher){
+        TeacherSearchResponse teacherSearchResponse = new TeacherSearchResponse();
+        if(teacher.getName() != null){
+            teacherSearchResponse.setName(teacher.getName());
+        }
+
+        if(teacher.getSchool() != null){
+            teacherSearchResponse.setSchool(teacher.getSchool().getSchoolName());
+        }
+
+        if(teacher.getYearsOfTeaching() != null){
+            teacherSearchResponse.setYearsOfTeaching(teacher.getYearsOfTeaching());
+        }
+        return teacherSearchResponse;
+    }
+
+}
