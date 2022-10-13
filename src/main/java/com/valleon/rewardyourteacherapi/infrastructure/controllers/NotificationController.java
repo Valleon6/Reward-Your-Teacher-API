72f523be0a453d80dcd9f2b5a77dc29b5abfe65d
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-13 13:14:45 +0100

    Create NotificationController.java

---
+++ src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/NotificationController.java
@@ -0,0 +1,33 @@
+package com.valleon.rewardyourteacherapi.infrastructure.controllers;
+
+import com.valleon.rewardyourteacherapi.service.payload.NotificationService;
+import com.valleon.rewardyourteacherapi.service.payload.request.NotificationRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/notification")
+public class NotificationController {
+    private final NotificationService notificationService;
+
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+    
+    @GetMapping("/student/{studentId}")
+    public ResponseEntity<List<NotificationRequest>> retrieveStudentNotification(@PathVariable("studentId") Long studentId){
+        return  new ResponseEntity<>(notificationService.allNotificationsOfA_StudentById(studentId), HttpStatus.FOUND);
+    }
+
+    @GetMapping("/teacher/{teacherId")
+    public ResponseEntity<List<NotificationRequest>> retrieveTeacherNotification(@PathVariable("teacherId") Long teacherId){
+        return new ResponseEntity<>(notificationService.allNotificationsOfA_TeacherById(teacherId), HttpStatus.FOUND);
+    }
+}
