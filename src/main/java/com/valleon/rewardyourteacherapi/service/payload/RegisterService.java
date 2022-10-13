03bb3ff1952b4c60685819b541bf36f5d8301b69
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-13 18:55:10 +0100

    Update RegisterService.java

--- src/main/java/com/valleon/rewardyourteacherapi/service/payload/RegisterService.java
+++ src/main/java/com/valleon/rewardyourteacherapi/service/payload/RegisterService.java
@@ -5,10 +5,11 @@ import com.valleon.rewardyourteacherapi.service.payload.request.TeacherRegistrat
 import com.valleon.rewardyourteacherapi.service.payload.response.RegistrationResponse;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 public interface RegisterService {
-    RegistrationResponse registerStudent(StudentRegistrationRequest studentRegistrationRequest);
+    RegistrationResponse registerStudent(StudentRegistrationRequest studentRegistrationRequest) throws Exception;
 
-    RegistrationResponse registerTeacher(TeacherRegistrationRequest teacherRegistrationRequest, MultipartFile file) throws IOException;
+    RegistrationResponse registerTeacher(TeacherRegistrationRequest teacherRegistrationRequest, MultipartFile file) throws Exception;
+
+    Object verifyUser(String userToken);
+
 }
