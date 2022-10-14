506dc51fb0f88ccb5e838c415c0a6c8fc774b901
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-14 12:35:13 +0100

    Update LoginController.java

--- src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/LoginController.java
+++ src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/LoginController.java
@@ -1,9 +1,10 @@
 package com.valleon.rewardyourteacherapi.infrastructure.controllers;
 
-import com.valleon.rewardyourteacherapi.service.payload.LoginService;
-import com.valleon.rewardyourteacherapi.service.payload.request.LoginRequest;
-import com.valleon.rewardyourteacherapi.service.payload.request.SocialLoginRequest;
-import com.valleon.rewardyourteacherapi.service.payload.response.LoginResponse;
+import com.valleon.rewardyourteacherapi.usecase.services.LoginService;
+import com.valleon.rewardyourteacherapi.usecase.payload.request.LoginRequest;
+import com.valleon.rewardyourteacherapi.usecase.payload.request.SocialLoginRequest;
+import com.valleon.rewardyourteacherapi.usecase.payload.response.ApiResponse;
+import com.valleon.rewardyourteacherapi.usecase.payload.response.LoginResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,23 +23,27 @@ public class LoginController {
     private final LoginService loginService;
 
     @PostMapping(value = "/student")
-    public ResponseEntity<LoginResponse> loginStudent(@Valid @RequestBody LoginRequest loginRequest){
-        return  new ResponseEntity<>(loginService.loginStudent(loginRequest), HttpStatus.ACCEPTED);
+    public ResponseEntity<ApiResponse<LoginResponse>>loginStudent(@Valid @RequestBody LoginRequest loginRequest){
+        LoginResponse response  = loginService.loginStudent(loginRequest);
+        return new ResponseEntity<>(new ApiResponse<>("Login Successful",true,response), HttpStatus.ACCEPTED);
     }
 
     @PostMapping("/teacher")
-    public ResponseEntity<LoginResponse> loginTeacher(@Valid @RequestBody LoginRequest loginRequest){
-        return new ResponseEntity<>(loginService.loginTeacher(loginRequest), HttpStatus.ACCEPTED);
+    public ResponseEntity<ApiResponse<LoginResponse>> loginTeacher(@Valid @RequestBody LoginRequest loginRequest){
+        LoginResponse response = loginService.loginTeacher(loginRequest);
+        return new ResponseEntity<>(new ApiResponse<>("Login Successful",true,response), HttpStatus.ACCEPTED);
     }
 
     @PostMapping("/social/student")
-    public ResponseEntity<LoginResponse> studentSocialLogin(@Valid @RequestBody SocialLoginRequest socialLoginRequest){
-        return new ResponseEntity<>(loginService.studentSocialLogin(socialLoginRequest), HttpStatus.ACCEPTED);
+    public ResponseEntity<ApiResponse<LoginResponse>>studentSocialLogin(@Valid @RequestBody SocialLoginRequest socialLoginRequest){
+        LoginResponse response = loginService.studentSocialLogin(socialLoginRequest);
+        return new ResponseEntity<>(new ApiResponse<>("Login Successful",true,response),HttpStatus.ACCEPTED);
     }
 
     @PostMapping("/social/teacher")
-    public ResponseEntity<LoginResponse> teacherSocialLogin(@Valid @RequestBody SocialLoginRequest socialLoginRequest){
-        return new ResponseEntity<>(loginService.teacherSocialLogin(socialLoginRequest), HttpStatus.ACCEPTED);
+    public ResponseEntity<ApiResponse<LoginResponse>> teacherSocialLogin(@Valid @RequestBody SocialLoginRequest socialLoginRequest){
+        LoginResponse response = loginService.teacherSocialLogin(socialLoginRequest);
+        return new ResponseEntity<>(new ApiResponse<>("Login Successful",true,response), HttpStatus.ACCEPTED);
     }
 
 }
