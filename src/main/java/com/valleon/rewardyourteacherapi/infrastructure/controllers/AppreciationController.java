86707ecae8812005ea6ecee2b10a9cd1ffdff0ba
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-14 12:34:22 +0100

    Update AppreciationController.java

--- src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/AppreciationController.java
+++ src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/AppreciationController.java
@@ -1,11 +1,12 @@
 package com.valleon.rewardyourteacherapi.infrastructure.controllers;
 
-import com.valleon.rewardyourteacherapi.service.impl.NotificationServiceImpl;
-import com.valleon.rewardyourteacherapi.service.payload.response.ApiResponse;
-import com.valleon.rewardyourteacherapi.service.payload.response.NotificationResponse;
+import com.valleon.rewardyourteacherapi.usecase.services.impl.NotificationServiceImpl;
+import com.valleon.rewardyourteacherapi.usecase.payload.response.ApiResponse;
+import com.valleon.rewardyourteacherapi.usecase.payload.response.NotificationResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +15,7 @@ public class AppreciationController {
 
     private final NotificationServiceImpl notificationService;
 
-
+    @PostMapping("/appreciate-student/{studentId}/{teacherId}")
     public ResponseEntity<ApiResponse<NotificationResponse>> teacherAppreciateStudent(@PathVariable (value = "studentId") Long studentId, @PathVariable(value = "teacherId") Long teacherId){
         return ResponseEntity.ok(new ApiResponse<>("Success", true, notificationService.studentAppreciatedNotification(studentId, teacherId)));
     }
