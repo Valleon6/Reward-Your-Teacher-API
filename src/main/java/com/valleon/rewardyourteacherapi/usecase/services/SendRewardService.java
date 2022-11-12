3791a7f533003818eb54b666906452b2a873fc8e
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-12 18:06:24 +0100

    Update SendRewardService.java

--- src/main/java/com/valleon/rewardyourteacherapi/usecase/services/SendRewardService.java
+++ src/main/java/com/valleon/rewardyourteacherapi/usecase/services/SendRewardService.java
@@ -7,7 +7,7 @@ import com.valleon.rewardyourteacherapi.usecase.payload.response.SendRewardRespo
 import java.math.BigDecimal;
 
 public interface SendRewardService {
-    SendRewardResponse sendRewardResponse(SendRewardRequest sendRewardRequest);
+    SendRewardResponse sendRewardResponse(SendRewardRequest sendRewardRequest) throws Exception;
 
     BigDecimal getStudentWalletBalance(Student student);
 }
