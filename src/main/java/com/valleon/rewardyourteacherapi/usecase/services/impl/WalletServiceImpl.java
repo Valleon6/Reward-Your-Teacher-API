14b631fe76ddb98763cede5e35cb43de8efacf75
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-12 18:29:21 +0100

    Update WalletServiceImpl.java

--- src/main/java/com/valleon/rewardyourteacherapi/usecase/services/impl/WalletServiceImpl.java
+++ src/main/java/com/valleon/rewardyourteacherapi/usecase/services/impl/WalletServiceImpl.java
@@ -4,26 +4,29 @@ import com.valleon.rewardyourteacherapi.domain.dao.*;
 import com.valleon.rewardyourteacherapi.domain.entities.AppUser;
 import com.valleon.rewardyourteacherapi.domain.entities.Student;
 import com.valleon.rewardyourteacherapi.domain.entities.Teacher;
+import com.valleon.rewardyourteacherapi.domain.entities.enums.NotificationType;
 import com.valleon.rewardyourteacherapi.domain.entities.enums.Role;
 import com.valleon.rewardyourteacherapi.domain.entities.enums.TransactionType;
+import com.valleon.rewardyourteacherapi.domain.entities.message.Notification;
 import com.valleon.rewardyourteacherapi.domain.entities.transact.Transaction;
 import com.valleon.rewardyourteacherapi.domain.entities.transact.Wallet;
 import com.valleon.rewardyourteacherapi.infrastructure.configuration.security.UserDetails;
 import com.valleon.rewardyourteacherapi.infrastructure.exceptionHandlers.CustomNotFoundException;
-import com.valleon.rewardyourteacherapi.usecase.services.PaymentService;
-import com.valleon.rewardyourteacherapi.usecase.services.WalletService;
 import com.valleon.rewardyourteacherapi.usecase.payload.request.EmailDetailsRequest;
 import com.valleon.rewardyourteacherapi.usecase.payload.request.FundWalletRequest;
 import com.valleon.rewardyourteacherapi.usecase.payload.request.PayStackTransactionRequest;
 import com.valleon.rewardyourteacherapi.usecase.payload.response.PayStackTransactionResponse;
 import com.valleon.rewardyourteacherapi.usecase.payload.response.PaymentResponse;
 import com.valleon.rewardyourteacherapi.usecase.payload.response.WalletResponse;
+import com.valleon.rewardyourteacherapi.usecase.services.PaymentService;
+import com.valleon.rewardyourteacherapi.usecase.services.WalletService;
 import com.valleon.rewardyourteacherapi.utilities.EmailService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 
 @Service
@@ -41,31 +44,54 @@ public class WalletServiceImpl implements WalletService {
     private final TransactionDao transactionDao;
 
     private final AppUserDao appUserDao;
+    private final NotificationDao notificationDao;
 
     @Override
     public WalletResponse getStudentWalletBalance() {
 
         String email = UserDetails.getLoggedInUserDetails();
         AppUser appUser = appUserDao.findAppUserByEmailAndRole(email, Role.STUDENT);
-        if(appUser == null){
+        if (appUser == null) {
             throw new CustomNotFoundException("User not found");
         }
 
         Student student = studentDao.getStudentByAppUser(appUser);
 
-        if(student == null){
+        if (student == null) {
             throw new CustomNotFoundException("Invalid user");
         }
         Wallet wallet = walletDao.findWalletByStudent(student);
-        if(wallet == null){
+        if (wallet == null) {
             throw new CustomNotFoundException("Wallet not found or phone number missing");
         }
 
-        return new WalletResponse(wallet.getBalance());
+        return new WalletResponse(wallet.getBalance(),wallet.getTotalMoneySent());
+    }
+
+    @Override
+    public WalletResponse getTeacherWalletBalance() {
+        String email = UserDetails.getLoggedInUserDetails();
+        AppUser appUser = appUserDao.findAppUserByEmailAndRole(email, Role.TEACHER);
+        if (appUser == null) {
+            throw new CustomNotFoundException("User not found");
+        }
+        Teacher teacher = teacherDao.getTeacherByAppUser(appUser);
+        if (teacher == null) {
+            throw new CustomNotFoundException("Invalid user");
+        }
+
+
+        Wallet wallet = walletDao.findWalletByTeacher(teacher)
+                .orElseThrow(() -> new CustomNotFoundException("Wallet not found or Phone number missing"));
+
+        return new WalletResponse(wallet.getBalance(),wallet.getTotalMoneySent());
     }
 
     @Override
     public PaymentResponse fundWallet(FundWalletRequest fundWalletRequest) throws Exception {
+
+        fundWalletRequest.setAmount(String.valueOf(Integer.parseInt(fundWalletRequest.getAmount())* 100));
+        fundWalletRequest.setAmount(fundWalletRequest.getAmount());
 
         String email = UserDetails.getLoggedInUserDetails();
         PayStackTransactionRequest payStackTransactionRequest = PayStackTransactionRequest
@@ -79,17 +105,22 @@ public class WalletServiceImpl implements WalletService {
         if (!transactionResponse.isStatus()) {
             throw new Exception("Payment not authorized");
         }
-        AppUser appUser = appUserDao.findAppUserByEmailAndRole(email,Role.STUDENT);
-        if(appUser == null){
+        AppUser appUser = appUserDao.findAppUserByEmailAndRole(email, Role.STUDENT);
+        if (appUser == null) {
             throw new CustomNotFoundException("Entity not found");
         }
+        fundWalletRequest.setAmount(String.valueOf(Integer.parseInt(fundWalletRequest.getAmount())/ 100));
+
+        double amount = Double.parseDouble(fundWalletRequest.getAmount());
+        DecimalFormat formatter = new DecimalFormat("#,###.00");
+
         Student student = studentDao.getStudentByAppUser(appUser);
         Wallet wallet = walletDao.findWalletByStudent(student);
         if (wallet == null) {
             Wallet walletDao1 = Wallet.builder()
                     .balance(new BigDecimal(fundWalletRequest.getAmount()))
                     .student(student)
-                    .totalMoneySent(new BigDecimal(fundWalletRequest.getAmount())).build();
+                    .build();
             walletDao.saveRecord(walletDao1);
             Transaction transaction = Transaction.builder()
                     .transactionType(TransactionType.CREDIT.name())
@@ -104,7 +135,6 @@ public class WalletServiceImpl implements WalletService {
         BigDecimal result = wallet.getBalance().add(new BigDecimal(fundWalletRequest.getAmount()));
         wallet.setBalance(result);
         wallet.setStudent(student);
-        wallet.setTotalMoneySent(new BigDecimal(fundWalletRequest.getAmount()));
         walletDao.saveRecord(wallet);
 
         Transaction transaction = Transaction
@@ -115,33 +145,22 @@ public class WalletServiceImpl implements WalletService {
                 .description(transactionResponse.getMessage())
                 .build();
         transactionDao.saveRecord(transaction);
+
+        Notification notification = Notification.builder()
+                .notificationType(NotificationType.CREDIT_NOTIFICATION)
+                .student(student)
+                .transaction(transaction)
+                .message("You Funded your wallet with ₦"+formatter.format(amount)).build();
+        notificationDao.saveRecord(notification);
+
         EmailDetailsRequest emailDetailsRequest = EmailDetailsRequest.builder()
-                .msgBody(emailService.WalletFundingEmail(student.getName(),fundWalletRequest.getAmount()))
+                .msgBody(emailService.WalletFundingEmail(student.getName(), fundWalletRequest.getAmount()))
                 .subject("Valleon6 email")
                 .recipient(appUser.getEmail())
                 .build();
         emailService.sendMailWithAttachment(emailDetailsRequest);
 
         return new PaymentResponse(transactionResponse.getData().getReference());
-    }
-
-    @Override
-    public WalletResponse getTeacherWalletBalance() {
-        String email = UserDetails.getLoggedInUserDetails();
-        AppUser appUser = appUserDao.findAppUserByEmailAndRole(email,Role.TEACHER);
-        if(appUser == null){
-            throw new CustomNotFoundException("User not found");
-        }
-        Teacher teacher = teacherDao.getTeacherByAppUser(appUser);
-        if(teacher == null){
-            throw new CustomNotFoundException("Invalid user");
-        }
-
-
-        Wallet wallet = walletDao.findWalletByTeacher(teacher)
-                .orElseThrow(()-> new CustomNotFoundException("Wallet not found or Phone number missing"));
-
-        return new WalletResponse(wallet.getBalance());
     }
 
 
