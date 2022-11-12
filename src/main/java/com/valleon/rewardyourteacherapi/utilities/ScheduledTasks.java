a4545d9de6a4dafc57e24fc4c78a307c016db0c5
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-12 18:53:30 +0100

    Update ScheduledTasks.java

--- src/main/java/com/valleon/rewardyourteacherapi/utilities/ScheduledTasks.java
+++ src/main/java/com/valleon/rewardyourteacherapi/utilities/ScheduledTasks.java
@@ -3,12 +3,9 @@ package com.valleon.rewardyourteacherapi.utilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 
 @Component
@@ -19,6 +16,6 @@ public class ScheduledTasks {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(initialDelay =  10, fixedRate = 10, timeUnit =  TimeUnit.SECONDS)
-    public void reportCurrentTime(){logger.info("The time is now {}", dateFormat.format(new Date()));}
+//    @Scheduled(initialDelay =  10, fixedRate = 10, timeUnit =  TimeUnit.SECONDS)
+//    public void reportCurrentTime(){logger.info("The time is now {}", dateFormat.format(new Date()));}
 }
