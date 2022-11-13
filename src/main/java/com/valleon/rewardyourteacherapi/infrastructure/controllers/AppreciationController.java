40e605dd1ff7a23fd6b706bacf793cc148e73cda
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-13 09:44:49 +0100

    Update AppreciationController.java

--- src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/AppreciationController.java
+++ src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/AppreciationController.java
@@ -7,17 +7,19 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/v1")
 public class AppreciationController {
 
     private final NotificationServiceImpl notificationService;
 
-    @PostMapping("/appreciate-student/{studentId}/{teacherId}")
-    public ResponseEntity<ApiResponse<NotificationResponse>> teacherAppreciateStudent(@PathVariable (value = "studentId") Long studentId, @PathVariable(value = "teacherId") Long teacherId){
-        return ResponseEntity.ok(new ApiResponse<>("Success", true, notificationService.studentAppreciatedNotification(studentId, teacherId)));
+    @PostMapping("appreciate-student/{transactionId}")
+    public ResponseEntity<ApiResponse<NotificationResponse>> teacherAppreciateStudent(@PathVariable ( value= "transactionId")Long transactionId){
+        return ResponseEntity.ok(new ApiResponse<>("Success",true,notificationService.studentAppreciatedNotification(transactionId)));
     }
 
 }
