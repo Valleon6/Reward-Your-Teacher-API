2a63ddccfc82a3b8a019a4cbfecb2923abda0f51
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-13 10:01:57 +0100

    Update RegisterController.java

--- src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/RegisterController.java
+++ src/main/java/com/valleon/rewardyourteacherapi/infrastructure/controllers/RegisterController.java
@@ -22,22 +22,30 @@ public class RegisterController {
     private final RegisterService registerService;
 
     @PostMapping(value = "/student")
-    public ResponseEntity<ApiResponse<RegistrationResponse>> registerStudent(@Valid @RequestBody StudentRegistrationRequest studentRegistrationRequest)throws Exception {
+    public ResponseEntity<ApiResponse<RegistrationResponse>> registerStudent(@Valid @RequestBody StudentRegistrationRequest studentRegistrationRequest) throws Exception {
         RegistrationResponse response = registerService.registerStudent(studentRegistrationRequest);
-        return new ResponseEntity<>(new ApiResponse<>("Registered successfully", true, response), HttpStatus.CREATED);
+        return new ResponseEntity<>(new ApiResponse<>("Registered successfully",true,response),HttpStatus.CREATED);
     }
 
     @PostMapping(value = "/teacher")
-    public ResponseEntity<ApiResponse<RegistrationResponse>> registerTeacher(@Valid @RequestParam("registration") String registration, @RequestParam("file") MultipartFile file) throws Exception {
+    public ResponseEntity<ApiResponse<RegistrationResponse>> registerTeacher(@Valid @RequestParam("registration") String registration,@RequestParam("file") MultipartFile file) throws Exception {
         TeacherRegistrationRequest teacherRegistrationRequest = new Gson().fromJson(registration,TeacherRegistrationRequest.class);
         RegistrationResponse response =  registerService.registerTeacher( teacherRegistrationRequest,file);
         return new ResponseEntity<>(new ApiResponse<>("Registered Successfully",true,response), HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/verification")
-    public ResponseEntity<ApiResponse<RegistrationResponse>> verifyUser(@Valid @RequestParam("token") String token) throws Exception {
+    public String verifyUser(@Valid @RequestParam("token") String token) throws Exception {
         registerService.verifyUser(token);
-        return new ResponseEntity<>(new ApiResponse<>("Registered Successfully",true), HttpStatus.CREATED);
+//        String link = "http://localhost:3000/login";
+//        String link = "www.google.com";
+        StringBuilder sb = new StringBuilder();
+        sb.append("Email confirmed, follow the link below to login.");
+        sb.append("<br><br><a href=\"http://localhost:3000/login\" target=\"_blank\">Login</a>");
+
+        String link = sb.toString();
+//        return "Registered successfully, follow the link to login, <a href=\""+ link +"\" target=\"_blank\">Login</a>";
+        return link;
     }
 
 }
